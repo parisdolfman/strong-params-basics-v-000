c4838059760cd1782ce14a7059e3cfa49fff5bd9
parisdolfman <parisdolfman@gmail.com>
2021-03-22 14:07:26 -0400

    commit tree 10537e604dd5ef7feec9e1bd818ad39d9ec72460

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module BlogFlash
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.action_controller.permit_all_parameters = true
+    # -- all .rb files in that directory are automatically loaded.  
   end
 end
